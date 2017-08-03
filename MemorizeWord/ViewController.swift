7420a6386f5e1dabeeb9c04dcb89606161c890f3
張凱慶 <kaiching@zhangkaiqingdeMacBook-Pro.local>
2017-08-03 10:00:57 +0800

    Initial Commit

---
+++ MemorizeWord/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MemorizeWord
+//
+//  Created by 張凱慶 on 03/08/2017.
+//  Copyright © 2017 kaiching. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
